34ba667d5b7260ea60c5f73bdb3b28538526ba03
Felipe Salas <felipe_jesus724@hotmail.com>
2022-06-14 14:04:03 -0600

    cambio de nombre carpeta ventas para mas claridad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,18 @@ import { AppComponent } from './app.component';
 
 // Módulo personalizado
 import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './practica/ventas.module';
 import { SharedModule } from './shared/shared.module';
-import { VentasModule } from './ventas/ventas.module';
 
 // Cambiar el locale de la app
+import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es-HN';
 import localeFr from '@angular/common/locales/fr';
-import { registerLocaleData } from '@angular/common';
-registerLocaleData( localeEs );
-registerLocaleData( localeFr );
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -27,9 +25,7 @@ registerLocaleData( localeFr );
     SharedModule,
     VentasModule,
   ],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'es-HN' }
-  ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: LOCALE_ID, useValue: 'es-HN' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
